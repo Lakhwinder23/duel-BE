c737ece2d2ac3003600e1e714ad257b449e27beb
Lakhwinder Singh <lakhi.8022@gmail.com>
2019-10-17 17:24:52 +0530

    Routes changes for password emails.

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 use Laravel\Passport\HasApiTokens;
+use App\Notifications\PasswordResetNotification;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -38,4 +39,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendPasswordResetNotification($token)
+   {
+       $this->notify(new PasswordResetNotification($token));
+   }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Http\Request;
 Route::post('login', 'Api\UserController@login');
 Route::post('register', 'Api\UserController@register');
 Route::get('logout/{id}', 'Api\UserController@Logout');
+Route::post('/password/email', 'Api\ForgotPasswordController@sendResetLinkEmail');
+Route::post('/password/reset', 'Api\ResetPasswordController@reset');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
